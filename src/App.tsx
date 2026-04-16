e5adcd1d467614f0f4fc2bd18ae72a816bd42e62
mwendell <mwendellsmce@gmail.com>
2026-04-16 20:24:50 -0300

    crud viagem inicial

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,31 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Navbar from './components/navbar/Navbar';
-import Footer from './components/footer/Footer';
-import Home from './pages/home/Home';
-import Introducao from './pages/introducao/Introducao';
-import Sobre from './pages/sobre/Sobre';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Navbar from './components/navbar/Navbar'
+import Footer from './components/footer/Footer'
+import Home from './pages/home/Home'
+import Introducao from './pages/introducao/Introducao'
+import Sobre from './pages/sobre/Sobre'
+import ListarViagens from './pages/viagens/ListarViagens'
+import FormViagem from './pages/viagens/FormViagem'
 
 function App() {
   return (
     <BrowserRouter>
       <div className="min-h-screen bg-astra-bg flex flex-col font-sans">
-        
         <Navbar />
-        
-        <main className="flex-grow container mx-auto px-4 py-8">
+        <main className="grow container mx-auto px-4 py-8">
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/introducao" element={<Introducao />} />
             <Route path="/sobre" element={<Sobre />} />
+            <Route path="/viagens" element={<ListarViagens />} />
+            <Route path="/viagens/form" element={<FormViagem />} />
+            <Route path="/viagens/form/:id" element={<FormViagem />} />
           </Routes>
         </main>
-
         <Footer />
-        
       </div>
     </BrowserRouter>
-  );
+  )
 }
 
-export default App;
+export default App
