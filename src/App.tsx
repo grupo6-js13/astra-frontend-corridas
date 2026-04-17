75e0027cd8e60e9e99d842375a0ede49f54668e1
Josue-Bravo <josuebc8@gmail.com>
2026-04-17 09:15:31 -0300

    Commit de merge

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Navbar from './components/navbar/Navbar'
 import Footer from './components/footer/Footer'
-import Home from './pages/home/Home'
-import Introducao from './pages/introducao/Introducao'
-import Sobre from './pages/sobre/Sobre'
+import { Home } from './pages/home/Home'
+import { Sobre } from './pages/sobre/Sobre'
 import ListarViagens from './pages/viagens/ListarViagens'
 import FormViagem from './pages/viagens/FormViagem'
 
@@ -15,8 +14,7 @@ function App() {
         <main className="grow container mx-auto px-4 py-8">
           <Routes>
             <Route path="/" element={<Home />} />
-            {/* <Route path="/viagens" element={<ListarViagens />} />
-            <Route path="/veiculos" element={<ListarVeiculos />} /> */}
+            {/* <Route path="/veiculos" element={<ListarVeiculos />} /> */}
             <Route path="/sobre" element={<Sobre />} />
             <Route path="/viagens" element={<ListarViagens />} />
             <Route path="/viagens/form" element={<FormViagem />} />
